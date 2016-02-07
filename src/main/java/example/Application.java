b8d9d795ad34ceb777e597deb4563e1f3e67ec3c
jojoldu <jojoldu@gmail.com>
2016-02-07 18:58:51 +0900

    #2-1 jdbcTemplate 로 전환

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import example.service.CustomerService;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 /**
  * Created by jojoldu@gmail.com on 2016-02-06.
@@ -19,6 +22,9 @@ public class Application implements CommandLineRunner{
     @Autowired
     CustomerService customerService;
 
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
     @Override
     public void run(String... args) throws Exception {
         customerService.save(new Customer(1, "동욱", "이"));
@@ -26,6 +32,16 @@ public class Application implements CommandLineRunner{
         customerService.save(new Customer(3, "우중", "정"));
 
         customerService.findAll().forEach(System.out::println);
+        System.out.println("Map에 담았다!");
+
+        String sql = "SELECT :a+:b";
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("a", 100)
+                .addValue("b", 200);
+
+        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+        System.out.println("result : " + result);
+
     }
 
     public static void main(String[] args) {
