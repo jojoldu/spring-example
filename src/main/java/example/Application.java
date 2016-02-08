10b664e955d0fb20f5e6d1ac43eff6e86e2cf438
jojoldu <jojoldu@gmail.com>
2016-02-08 22:22:40 +0900

    Pageable 사용하여 페이징 기능 깔짝 사용

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -8,6 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import example.service.CustomerService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -30,6 +33,13 @@ public class Application implements CommandLineRunner{
     public void run(String... args) throws Exception {
         Customer created = customerRepository.save(new Customer(null, "동욱","이"));
         System.out.println(created + " is created!");
+
+        Pageable pageable = new PageRequest(0, 3);
+        Page<Customer> page = customerRepository.findAll(pageable);
+        System.out.println("1페이지당 데이터수 : " + page.getSize());
+        System.out.println("현재페이지 : " + page.getNumber());
+        System.out.println("전체 페이지수 : " + page.getTotalPages());
+        System.out.println("전체 데이터수 : " + page.getTotalElements());
         customerRepository.findAll().forEach(System.out::println);
     }
 
